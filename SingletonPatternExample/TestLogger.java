75771f8f9764504b34beeab50f2ff14ccb9d8b57
SubhajitMitra-GH <msubhojit182004@gmail.com>
2026-06-21 21:19:21 +0530

    Removed unnecessary stuffs

--- SingletonPatternExample/TestLogger.java
+++ SingletonPatternExample/TestLogger.java
@@ -4,15 +4,12 @@ public class TestLogger {
 
     public static void main(String[] args) {
 
-        // Get Logger instance for the first time
         Logger logger1 = Logger.getInstance();
         logger1.log("First message");
 
-        // Get Logger instance again
         Logger logger2 = Logger.getInstance();
         logger2.log("Second message");
 
-        // Verify that both references point to the same object
         if (logger1 == logger2) {
             System.out.println("Only one Logger instance exists.");
         } else {
