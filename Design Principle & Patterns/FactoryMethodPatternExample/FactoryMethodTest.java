c2cd6674e7523ab3cf5facdfc3c6ae786f2dac09
SubhajitMitra-GH <msubhojit182004@gmail.com>
2026-06-21 21:25:12 +0530

    Folder created

--- Design Principle & Patterns/FactoryMethodPatternExample/FactoryMethodTest.java
+++ Design Principle & Patterns/FactoryMethodPatternExample/FactoryMethodTest.java
@@ -3,17 +3,14 @@ package FactoryMethodPatternExample;
 public class FactoryMethodTest {
 
     public static void main(String[] args) {
-        // Create WordDocument using WordDocumentFactory
         DocumentFactory wordFactory = new WordDocumentFactory();
         Document wordDoc = wordFactory.createDocument();
         wordDoc.open();
 
-        // Create PdfDocument using PdfDocumentFactory
         DocumentFactory pdfFactory = new PdfDocumentFactory();
         Document pdfDoc = pdfFactory.createDocument();
         pdfDoc.open();
 
-        // Create ExcelDocument using ExcelDocumentFactory
         DocumentFactory excelFactory = new ExcelDocumentFactory();
         Document excelDoc = excelFactory.createDocument();
         excelDoc.open();
