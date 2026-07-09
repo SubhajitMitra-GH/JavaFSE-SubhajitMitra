a4812a0cd06b2df9377d614f7b1ad19eec1bfbd5
SubhajitMitra-GH <msubhojit182004@gmail.com>
2026-07-09 17:24:16 +0530

    week 3

---
+++ Spring Data JPA with Spring Boot, Hibernate/orm-learn/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+-- Create Schema
+CREATE DATABASE IF NOT EXISTS ormlearn;
+USE ormlearn;
+
+-- Create Country Table
+CREATE TABLE IF NOT EXISTS country (
+    co_code VARCHAR(2) PRIMARY KEY,
+    co_name VARCHAR(50)
+);
+
+-- Populate Country Table
+INSERT INTO country (co_code, co_name) VALUES ('IN', 'India') ON DUPLICATE KEY UPDATE co_name='India';
+INSERT INTO country (co_code, co_name) VALUES ('US', 'United States of America') ON DUPLICATE KEY UPDATE co_name='United States of America';
