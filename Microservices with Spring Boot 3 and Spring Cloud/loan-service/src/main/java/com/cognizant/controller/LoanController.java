475778203a4d5b1f6744aa04488fa7aee8518488
SubhajitMitra-GH <msubhojit182004@gmail.com>
2026-07-21 16:15:24 +0530

    week 4

---
+++ Microservices with Spring Boot 3 and Spring Cloud/loan-service/src/main/java/com/cognizant/controller/LoanController.java
@@ -0,0 +1,15 @@
+package com.cognizant.controller;
+
+import com.cognizant.model.Loan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Loan getLoanDetails(@PathVariable String number) {
+        return new Loan(number, "car", 400000.0, 3258.0, 18);
+    }
+}
